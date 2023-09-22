2e4bf68900bcca94f07f80a7185513d9b3c70c04
akautomatadev <automatalab.contacto@gmail.com>
2023-09-22 10:49:15 -0600

    models

--- models/index.js
+++ models/index.js
@@ -1,13 +1,28 @@
 const User = require('./User');
-const Project = require('./Project');
+const Blog = require('./Blog');
+const Comment = require('./Comment.js');
 
-User.hasMany(Project, {
+User.hasMany(Blog, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
-Project.belongsTo(User, {
+Blog.hasMany(Comment, {
+  foreignKey: 'blog_id',
+  onDelete: 'CASCADE'
+});
+
+Blog.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Project };
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Blog, {
+  foreignKey: 'blog_id'
+});
+
+
+module.exports = { User, Blog, Comment };
